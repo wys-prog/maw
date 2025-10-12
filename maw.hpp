62a8542b3ed7ff77f5ea3225c84194b31f08778e
wys-prog <wys-workshop@outlook.fr>
2025-10-12 23:00:08 +0200

    adding simple macros

--- maw.hpp
+++ maw.hpp
@@ -58,7 +58,23 @@
       };                                                    \
     return info;                                            \
   }
-
+#define MAW_DefMethod(CLASS, NAME, ARGS, BODY)                                  \
+  std::shared_ptr<maw::object> NAME ARGS BODY                                   \
+  struct _maw_reg_##CLASS##_##NAME##_struct {                                   \
+    _maw_reg_##CLASS##_##NAME##_struct() {                                      \
+      const maw::type_info &info = CLASS().get_type();                          \
+      const_cast<std::unordered_map<std::string, std::shared_ptr<maw::invocable>>&>(info.methods)[#NAME] = \
+        std::make_shared<maw::invocable>([](maw::object_argv argv) -> maw::shared_object { \
+          if (argv.empty()) throw maw::bad_invocation({"argc: expected self"});  \
+          auto self = std::dynamic_pointer_cast<CLASS>(argv[0]);                \
+          if (!self) throw maw::invalid_type({"argv[0]", "expected " #CLASS});  \
+          /* call the actual C++ method (unwrap args manually here if needed) */ \
+          if constexpr (std::is_same_v<decltype(&CLASS::NAME), std::shared_ptr<maw::object>(CLASS::*)(void)>) \
+            return self->NAME();                                                \
+          else return self->NAME(argv);                                         \
+        });                                                                     \
+    }                                                                           \
+  } _maw_reg_instance_##CLASS##_##NAME;
 #define MAW_GetSourceString(T) std::string(m_type_name(T)) + "." + std::string(__func__)
 #define MAW_GetSourceStringNotFunc(T) std::string(std::string(#T)) + "." + std::string(__func__)
 #define MAW_GetSrouceStringClass() MAW_GetSourceString(*this)
@@ -79,7 +95,7 @@
 #ifdef MAW_USES_LANGUAGE_MACROS
 # define cla(N) class N : public maw::object
 # define let    auto
-# define fun(T) T
+# define fun(IN, N, ARG, BODY) MAW_DefMethod(IN, N, ARG, BODY)
 # define pub    public:
 # define prv    private:
 # define pro    protected:
